b414cf82df9a98ccfb1b92bd4f80c28cf0b9c819
davis-french <Davis.French@eltoro.com>
2017-05-12 13:09:33 -0400

    more tutorial code and notes

--- client/App.js
+++ client/App.js
@@ -19,7 +19,7 @@ import React from 'react'
 
 
 class App extends React.Component {
-  render(){
+/*  render(){
     let txt = this.props.txt //let used to declare a variable that is limited in scope within the area it is defined
     return <h1>{txt}</h1>
   }
@@ -28,7 +28,51 @@ class App extends React.Component {
 App.propTypes = {
   txt: React.PropTypes.string, //want txt to be of type string
   cat: React.PropTypes.number
+}*/
+  /*constructor(){
+    super();
+    this.state = { //the this here is to differentiate it from parent class. Want to refer to THIS particular instance
+      txt: 'this is text'
+    }
+  }
+  update (e){
+    this.setState({txt: e.target.value}) //the this here refers to the state in the constructor
+  }*/
+  /*render(){
+    return ( //the this in here refers to the update function
+      <div>
+        <input type="text" onChange={this.update.bind(this)}/>
+        <h1>{this.state.txt}</h1>
+      </div>
+    )
+  }
+}*/
+/*render(){
+  return ( //the this in here refers to the update function
+    <div>
+      <h1>{this.state.txt}</h1>
+      <Widget update={this.update.bind(this)} />
+      <Widget update={this.update.bind(this)} />
+      <Widget update={this.update.bind(this)} />
+    </div>
+  ) //order of h1 and widget etc changes order displayed
+} //all update and none take priority
+}*/
+
+//const Widget = (props) =>
+//  <input type="text" onChange={props.update}/>
+  render(){
+    return <Button>  React </Button> //want to access React
+  }
 }
+
+const Button = (props) => <button>{props.children}</button> //this gets React stateless so uses props?
+//in this case Button is prop so the props.children are any of the elements text etc enclosed within the Button tags
+/*class Heart extends React.Component {
+  render(){
+    return <span>&hearts;</span>
+  }
+}*/
 
 export default App //this shit does not print and i don't know why this is from video four
 
--- client/main.js
+++ client/main.js
@@ -3,9 +3,13 @@ import { render } from 'react-dom'
 //import ReactDOM from 'react-dom';
 import App from './App'
 
-/*Meteor.startup(() => {
-  render(<App />, document.getElementById("render-target")); //target
-});*/
+Meteor.startup(() => { //always need
+  render(
+    <App txt="this is a prop" />,
+    document.getElementById('root')
+  );
+  //render(<App />, document.getElementById("render-target")); //target
+});
 //Returns a reference to the element by its ID; the ID is a string which can be used to uniquely identify the element, found in the HTML id attribute.
 //so this returns a reference
 //render-target is the target so <App /> is what's being sent to the target
@@ -20,7 +24,3 @@ render(){
 }*/
 //pass data into components using props
 //ReactDOM.render(
-render(
-  <App cat={5} txt="this is a prop" />,
-  document.getElementById('root')
-);
