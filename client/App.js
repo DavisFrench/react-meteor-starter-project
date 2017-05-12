0c76031350a217611f348dba4bedc73f4704723e
davis-french <Davis.French@eltoro.com>
2017-05-11 22:35:06 -0400

    no idea why this wont display. tutorial guy was real helpful only showing changes in two of his three files. I think the error has something to do w/statement in div tags and what is getting targeted, but I'm not sure

--- client/App.js
+++ client/App.js
@@ -1,5 +1,37 @@
 import React from 'react'
 
-const App = () => <h1>Hello World</h1>
+//const App = () => <div>5+3 </div> //does not perform operation
+//function takes no parameters and has html components in func definition
+//class App extends React.Component {
+  //render(){
+    //return <h1> actual class </h1> //can give html tag a class BUT class is a keyword, so you have to use <h1 className="blah">
+    //return React.createElement('h1',null,'non jsx boi')
+    //element is first argument(remember if element starts with a capital it is custom)
+    //2nd argument for props
+    //3rd can be another element or string rep inner element of tag
+    //this is non jsx but equiv
+  //}
+//} // ^ all this crap works
 
-export default App;
+//const App = () => <h1> stateless and proud </h1>
+//stateless func cant have state, duh
+//but a class can
+
+
+class App extends React.Component {
+  render(){
+    let txt = this.props.txt //let used to declare a variable that is limited in scope within the area it is defined
+    return <h1>{txt}</h1>
+  }
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string, //want txt to be of type string
+  cat: React.PropTypes.number
+}
+
+export default App //this shit does not print and i don't know why this is from video four
+
+//okay yay this worked
+//seemingly this gets rendered and sent to main.js
+//then the html file is what does the actual displaying
--- client/main.js
+++ client/main.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import { render } from 'react-dom'
+//import ReactDOM from 'react-dom';
 import App from './App'
 
-Meteor.startup(() => {
-  render(<App />, document.getElementById("render-target"));
-});
+/*Meteor.startup(() => {
+  render(<App />, document.getElementById("render-target")); //target
+});*/
+//Returns a reference to the element by its ID; the ID is a string which can be used to uniquely identify the element, found in the HTML id attribute.
+//so this returns a reference
+//render-target is the target so <App /> is what's being sent to the target
+
+//console.log("hello testing regular js") //this doesn't do anything so I'm guessing it needs a render
+/*class test extends React.Component{
+render(){
+  return( //returns dom representation of component
+    <h1> hey </h1>
+  );
+}
+}*/
+//pass data into components using props
+//ReactDOM.render(
+render(
+  <App cat={5} txt="this is a prop" />,
+  document.getElementById('root')
+);
